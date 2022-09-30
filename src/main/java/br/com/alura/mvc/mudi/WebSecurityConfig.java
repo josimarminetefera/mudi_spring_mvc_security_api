73888a1376c288bf120dc18f93c3ee7862c66424
Josimar Minete <josimaminete@gmail.com>
2022-09-30 09:42:32 -0300

    Adiciona logout e melhorar códigos.

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,37 +1,65 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration // classe de configuração do spring
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	// configuração de autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		System.out.println("----------------- WebSecurityConfig Adapter configure");
 		// http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 		
-		http.authorizeRequests()
-			.anyRequest().authenticated()
+		http.authorizeRequests() // obriga entrar autorizado
+			.anyRequest() // qualquer request
+			.authenticated() // tem que estar autorizado
 			.and()
-			.formLogin(form -> form.loginPage("/login").permitAll())
-			.logout(logout -> logout.logoutUrl("/logout"));
+			.formLogin(form -> 
+				form.loginPage("/login")
+				.permitAll() // login não precisa estar logado
+			)
+			.logout(logout -> 
+				logout.logoutUrl("/logout")//deslogar o usuário
+			);
 	}
-
-	@Bean
+	
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("joao").password("joao").roles("ADM").build();
-
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("----------------- WebSecurityConfig AuthenticationManagerBuilder configure");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); // o que gera a criptografia boa
+		
+		//Criar usuário inicial
+		//UserDetails user = User.builder().username("joao").password(encoder.encode("joao")).roles("ADM").build();
+		
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(encoder);
+		//.withUser(user);
 	}
+
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user = User.withDefaultPasswordEncoder().username("joao").password("joao").roles("ADM").build();
+//		return new InMemoryUserDetailsManager(user);
+//	}
 }
--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,16 +1,41 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.mvc.mudi.model.Pedido;
+
 @Controller
 public class HelloController {
 
-	//http://localhost:8080/hello
+	// Testando comunicação com o banco de dados
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	// http://localhost:8080/hello
 	@GetMapping("/hello")
 	public String hello(Model model) {
+		System.out.println("----------------- HelloController /hello");
 		model.addAttribute("nome", "Mundo");
+		// adicionando atributos
+		model.addAttribute("nome", "Peste do Mal");
+		model.addAttribute("teste", "Teste");
+
+		// Testando consulta aos pedidos usando EntityManager
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		pedidos.forEach(p -> System.out.println(p.getNomeProduto()));
+		for (Pedido item : pedidos) {
+			System.out.print(item.getNomeProduto());
+		}
+
 		return "hello";
 	}
 }
